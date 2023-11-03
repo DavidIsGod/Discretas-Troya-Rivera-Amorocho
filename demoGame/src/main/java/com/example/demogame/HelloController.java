afaceec9c1775a977b748380c97810fd73978048
Administrator <juancamiloamorocho@hotmail.com>
2023-11-02 17:26:01 -0700

    Screen register

--- demoGame/src/main/java/com/example/demogame/HelloController.java
+++ demoGame/src/main/java/com/example/demogame/HelloController.java
@@ -2,6 +2,8 @@ package com.example.demogame;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
 
 public class HelloController {
     @FXML
@@ -10,5 +12,13 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    private TextField nombreTextField;
+
+    public void registrarJugador(ActionEvent event) {
+        String nombre = nombreTextField.getText();
+        System.out.println("Jugador registrado: " + nombre);
     }
 }
